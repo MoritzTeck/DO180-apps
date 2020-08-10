6ff0df07293a8ec39b52a32dae64b6b94d5b153d
MoritzTeck <moritz@teckenbrock.info>
2020-08-10 10:48:11 +0200

    Another change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
 print "A 2nd change is being worked!\n";
-=======
-print "A change is coming!\n";
->>>>>>> b0e74e23f78fcb2365e1686abd582c918eb5a600
 ?>
